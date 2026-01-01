b4950bd460a9dd09dd0da62812c3a77722e3fdce
Sibil Sarjam Soren <sibil.sorenofficial@gmail.com>
2026-01-01 23:20:38 +0530

    feat: implement LinkedList and LinkedListNode classes

---
+++ practice/LinkedList/LinkedList.js
@@ -0,0 +1,67 @@
+import Comparator from '../../src/utils/comparator/Comparator';
+import LinkedListNode from './LinkedListNode';
+
+export default class LinkedList {
+  constructor(comparatorFunction) {
+    this.head = null;
+    this.tail = null;
+
+    this.compare = new Comparator(comparatorFunction);
+  }
+
+  prepend(value) {
+    const newNode = new LinkedListNode(value, this.head);
+    this.head = newNode;
+
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+    return this;
+  }
+
+  append(value) {
+    const newNode = new LinkedListNode(value);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+
+      return this;
+    }
+
+    this.tail.next = newNode;
+    this.tail = newNode;
+
+    return this;
+  }
+
+  insert(value, rawIndex) {
+    const index = rawIndex < 0 ? 0 : rawIndex;
+
+    if (index === 0) {
+      this.prepend(value);
+    } else {
+      let count = 1;
+      let currentNode = this.head;
+      const newNode = new LinkedListNode(value);
+      while (currentNode) {
+        if (index === count) break;
+        currentNode = currentNode.next;
+        count += 1;
+      }
+      if (currentNode) {
+        newNode.next = currentNode.next;
+        currentNode.next = newNode;
+      } else {
+        if (this.tail) {
+          this.tail.next = newNode;
+          this.tail = newNode;
+        } else {
+          this.head = newNode;
+          this.tail = newNode;
+        }
+      }
+    }
+    return this;
+  }
+}
